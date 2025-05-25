a2ac91812e31764ca0e247734e67714b36ba37a6
Anton Liakh <ant.pochta@gmail.com>
2025-05-25 16:58:24 +0300

    fix: remove external fonts; add lazy load to img

--- Modules/Agency/resources/views/agencies.blade.php
+++ Modules/Agency/resources/views/agencies.blade.php
@@ -22,7 +22,7 @@
                 </ol>
             </nav>
         </header>
-        <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+        <div class="relative min-h-screen flex flex-col items-center justify-top selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <main class="mt-6">
                     <div class="grid gap-6 lg:grid-cols-1 lg:gap-8">
@@ -32,7 +32,7 @@
                             class="flex justify-between items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                         >
                             <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                                <img class="rounded-full w-10" src="/storage/agencies/{{ $agency->logo }}" alt="{{ $agency->name }}">
+                                <img loading="lazy" class="rounded-full w-10" src="/storage/agencies/{{ $agency->logo }}" alt="{{ $agency->name }}">
                             </div>
 
                             <div class="grow pt-3 sm:pt-5">
--- Modules/Agency/resources/views/components/layouts/master.blade.php
+++ Modules/Agency/resources/views/components/layouts/master.blade.php
@@ -14,8 +14,6 @@
         <meta name="author" content="{{ $author ?? '' }}">
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
