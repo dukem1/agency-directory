7aa51f2de802f429ce91c9465df39adcaffff54b
Anton Liakh <ant.pochta@gmail.com>
2025-05-25 17:25:20 +0300

    feat: add cache headers for agencies

--- Modules/Agency/routes/web.php
+++ Modules/Agency/routes/web.php
@@ -5,7 +5,10 @@ use Modules\Agency\Actions\GetAgencies;
 use Modules\Agency\Actions\GetAgency;
 use Modules\Agency\Http\Controllers\AgencyController;
 
-Route::middleware([\Fahlisaputra\Minify\Middleware\MinifyHtml::class])->group(function () {
+Route::middleware([
+    \Fahlisaputra\Minify\Middleware\MinifyHtml::class,
+    'cache.headers:public;max_age=86400;etag'
+])->group(function () {
 //    Route::resource('agencies', AgencyController::class)->names('agency');
     Route::get('agencies', GetAgencies::class)->name('agency.list');
     Route::get('agencies/{agency}', GetAgency::class)->name('agency.agency');
